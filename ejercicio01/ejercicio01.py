d75229d944f93c0c9ee4ec703c7bb65d76383e5e
BobiStudio <mgonzgarc@myuax.com>
2023-03-12 17:43:25 +0100

    arreglo

--- ejercicio01/ejercicio01.py
+++ ejercicio01/ejercicio01.py
@@ -13,17 +13,18 @@ Se pide resolver el mismo problema definiendo una función recursiva.
 Puede encontrar una solución estudiada de este ejercicio en los complementos disponibles para descargar desde la página Información.
 '''
 
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-lista = lista.sort()  # Ordenar la lista
-valor = 5
-def busqueda_dicotomia(lista, valor):
-    if len(lista) == 0:
-        return False
-    else:
-        medio = len(lista) // 2 # Dividir la lista en dos
-        if lista[medio] == valor: # Si el valor está en la mitad
-            return medio
-        elif lista[medio] > valor: # Si el valor está en la primera mitad
-            return busqueda_dicotomia(lista[:medio], valor) #LISTA[0,valor medio]
-        else: # Si el valor está en la segunda mitad
-            return busqueda_dicotomia(lista[medio+1:], valor) #LISTA[valor medio + 1,longitud lista]
+def busqueda_dicotomia(lista, elemento, inicio, fin):
+    if inicio > fin:
+        return "No hay ningún elemento en la posición seleccionada"
+    else: #Dividir la lista en dos partes: Concepto dicotomía
+        mitad = (inicio + fin) // 2
+        if lista[mitad] == elemento:
+            return mitad
+        elif lista[mitad] > elemento:
+            return busqueda_dicotomia(lista, elemento, inicio, mitad - 1)
+        else:
+            return busqueda_dicotomia(lista, elemento, mitad + 1, fin)
+
+if __name__ == "__main__":
+    lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(busqueda_dicotomia(lista, 7, 0, len(lista)))
